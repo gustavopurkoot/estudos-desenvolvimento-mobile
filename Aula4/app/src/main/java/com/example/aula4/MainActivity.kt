bbb98df56532a29159d4f15918ac1e5dcd3b3a4d
gustavopurkoot <gustavopurkoot04053357@gmail.com>
2026-08-18 11:53:18 -0300

    feat: adiciona aula 4

---
+++ Aula4/app/src/main/java/com/example/aula4/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.aula4
+
+import android.R.attr.text
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aula4.ui.theme.Aula4Theme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            Aula4Theme {
+                //MinhaTela()
+                Formulario()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Aula4Theme {
+        Greeting("Mundo!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MinhaTela() {
+    var contador by remember {mutableStateOf(0)}
+    Column {
+        Text("Contagem: $contador")
+
+        Button(
+            onClick = {
+                contador++
+                Log.d("A", "Apertou o botão - $contador")
+            }
+        ) {
+            Text("APERTE AQUI")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Formulario(){
+    Column {
+        Text(" - CADASTRO - ")
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = { Text("Nome:") }
+        )
+
+        Button(
+            onClick = { }
+        ) {
+            val nome = editNome.text.toString()
+            val email = editEmail.text.toString()
+            Text("Enviar")
+        }
+    }
+}
